ee4adfe0381740f5b39cdc9ca865033796c17022
atul mouraya <atul.mouraya94@gmail.com>
2023-03-28 22:53:28 +0530

    initial commit

---
+++ src/main/java/com/atul/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.atul.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	
+	@GetMapping("/admin")
+	public String adminHome() {
+		
+		return "adminHome";
+		
+	}
+	
+
+
+}
